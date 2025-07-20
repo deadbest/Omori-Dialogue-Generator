5b372985e517be426e57e09e1a9af3ad06c82e32
deadbest <best-trainer@hotmail.com>
2025-07-20 13:21:34 +0530

    Fix download link for mobile scenario

--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,3 @@
-const normalFont = new FontFace("OMORI_MAIN", "url(./css/fonts/OMORI_GAME.ttf)");
-const disturbedFont = new FontFace("OMORI_DISTURBED", "url(./css/fonts/OMORI_GAME2.ttf)");
-// const disturbedFont = new FontFace("OMORI_MAIN", "url(./css/fonts/OMORI_GAME.ttf)");
-document.fonts.add(normalFont);
-document.fonts.add(disturbedFont);
-console.log("Fonts loaded");
-
 function loadHtml2Canvas(callback) {
   const script = document.createElement('script');
   script.src = "https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js";
@@ -15,15 +8,17 @@ function loadHtml2Canvas(callback) {
 function downloadDialogue(idDiv) {
   loadHtml2Canvas(() => {
     html2canvas(document.getElementById(idDiv), {
-      scale: 3,
-      useCORS: true,
+      scale: 2,
+      // useCORS: true,
       backgroundColor: null
     }).then(canvas => {
-      // Convert canvas to image
       const link = document.createElement("a");
       link.download = "omori-dialogue.png";
       link.href = canvas.toDataURL("image/png");
+      link.target = "_blank";
+      document.body.appendChild(link);
       link.click();
+      document.body.removeChild(link);
     });
   });
 }
